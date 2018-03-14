ca9350a28536474e22ad2e88e50fa1f7d221fe59
atul-github <atul_pandit@yahoo.com>
2018-03-14 11:53:49 +0530

    explicit composite and util

--- index.js
+++ index.js
@@ -3,48 +3,67 @@ const DataSource = loopback.DataSource;
 const DataAccessObject = DataSource.DataAccessObject;
 const utils = require('loopback-datasource-juggler/lib/utils');
 const implicitComposite = require('./lib/implicit-composite');
+const explicitComposite = require('./lib/explicit-composite');
 
 const _create = DataAccessObject.create;
 DataAccessObject.create = function (data, options, cb) {
   var self = this;
-
-  if (!cb){
-    cb = options;
+  var settings = self.definition.settings;
+  // Atul : If compositeModel is being posted.. call compositePost() helper
+  if (settings.compositeModels) {
+      explicitComposite.compositePost(self, data, options, cb);
+      return data;
   }
-
-  cb = cb || utils.createPromiseCallback();
-
-  //Atul : relation is object that fetches all child relation of model for which data is being posted.
-  var relations = {};
-  for (var r in self.relations) {
-      if (self.relations[r].type === 'hasMany' || self.relations[r].type === 'hasOne' || self.relations[r].type === 'belongsTo') {
-          if (self.relations[r].modelTo) {
-              relations[r] = self.relations[r].modelTo.modelName;
-          }
-      }
-  }
-  // Atul : childData holds nested related data of model. only one level nesting is supported
-  var childData = false;
-  for (var relName in relations) {
-      if (!data[relName])
-          continue;
-      if (typeof data[relName] == 'function')
-          continue;
-      if (!childData)
-          childData = {};
-      childData[relName] = data[relName];
-      delete data[relName];
-  }
-
+  
+  var d = implicitComposite.getChildDataAndRelations(self, data);
   // Atul : if nested data exist, fall in to this if and create records recursively for nested child
-  if (childData) {
-    var m = self.modelName;
-    implicitComposite.create(self, data, childData, relations, options, cb);
+  if ( d.childData ){
+    if(!cb && typeof options === 'function'){
+      cb = options;
+      options = {};
+    }
+    cb = cb || utils.createPromiseCallback();
+    implicitComposite.create(self, data, d.childData, d.relations, options, cb);
     return cb.promise;
   }
 
+  // call original create function
   return _create.apply(this, [].slice.call(arguments));
 }
+
+const _upsert = DataAccessObject.upsert;
+DataAccessObject.updateOrCreate = DataAccessObject.upsert = function upsert(data, options, cb) {
+  var self=this;
+  var d = implicitComposite.getChildDataAndRelations(self, data);
+  // Atul : if nested data exist, fall in to this if and create records recursively for nested child
+  if ( d.childData ){
+    if(!cb && typeof options === 'function'){
+      cb = options;
+      options = {};
+    }
+    cb = cb || utils.createPromiseCallback();
+    implicitComposite.upsert(self, data, d.childData, d.relations, options, cb);
+    return cb.promise;
+  }
+
+  // call original create function
+  return _upsert.apply(this, [].slice.call(arguments));
+}
+
+const _updateAttributes = DataAccessObject.prototype.updateAttributes;
+DataAccessObject.prototype.updateAttributes = function updateAttributes(data, options, cb) {
+  var self=this;
+
+  var d = implicitComposite.getChildDataAndRelations(self, data);
+  // Atul : if nested data exist, fall in to this if and create records recursively for nested child
+  if ( d.childData ){
+    implicitComposite.updateAttributes(self, data, d.childData, d.relations, options, cb);
+    return cb.promise;
+  }  
+
+  return _updateAttributes.apply(this, [].slice.call(arguments));
+}
+
 
 
 
